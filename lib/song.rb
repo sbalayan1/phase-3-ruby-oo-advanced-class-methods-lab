2daf13c349f169ffc4dbd02b1c27184ed1166821
sbalayan1 <balayans2014@yahoo.com>
2021-07-02 11:02:48 -0700

    commit tree 1ce28355f4d787e6d05a675f64230033b11212ab

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +12,23 @@ class Song
     self.class.all << self
   end
 
+  # Build a class constructor Song.create that initializes a song and saves it to the @@all class variable either literally or through the class method Song.all. This method should return the song instance that was initialized and saved.
+  def self.create
+    #initialize the song 
+    song = self.new
+    song
+    @@all << song
+  end
+
+  def self.new_by_name name
+    song = self.all.new
+    song.name = name
+    @@all << song
+  end
+
+  def self.create_by_name name
+    song = self.new
+    song.name = name
+    @@all << song
+  end 
 end
